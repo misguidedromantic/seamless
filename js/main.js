a539a91abb74131a6c1c1b23d775f58bf5ea148e
misguidedromantic <122736106+misguidedromantic@users.noreply.github.com>
2025-09-12 08:21:10 +1000

    refactored for menuItem class

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 window.onload = async function(){
     
     display.setup()
-    loadSME()
+    displays.loadSME()
+    //loadSME()
     
 
     //render SME, render SME slection options
 
 }
+
+
 
 
 
@@ -21,6 +24,22 @@ function loadSME (){
             new sme ('Freelance Profressional')
         ]
     }
+
+    
+
+    function setupSMEmenu(){
+        
+        const SMEs = [
+            'Side Hustle',
+            'Construction Company',
+            'Freelance Profressional'
+        ]
+
+
+
+
+    }
+    
 
     
     function render(svg, data){
@@ -98,11 +117,138 @@ function loadSME (){
     
     
     const svg = display.getCanvas()
+    
+    
+    
     const data = getSMEdata()
     render(svg, data)
 }
 
 
+class displays {
+    static sme = {}
+
+    static loadSME (){
+        const SMEs = [
+            'Side Hustle',
+            'Construction Company',
+            'Freelance Profressional'
+        ]
+
+        this.sme = new menu ('SME', SMEs)
+        this.sme.render()
+
+    }
+
+}
+
+class menu {
+    items = []
+    maxHeight = 0
+    maxWidth = 0
+
+    constructor(title, options){
+        this.title = title
+        this.createItems(options)
+    }
+
+    createItems(options){
+        this.items.push(new menuItem ('title', this.title))
+        this.items.push(new menuItem ('selector', 'click to select'))
+
+        options.forEach(option => {
+            this.items.push(new menuItem ('selectable', option))
+        });
+    }
+
+    getSelectedItemIndex(){
+        return this.items.findIndex(item => item.selected === true)
+    }
+
+    render(svg = d3.select('#displaySvg')){
+
+        const positioning = new menuItemPositioning (this.items)
+        const styling = new menuItemStyling (this.items)
+        
+        svg.selectAll('g.sme')
+            .data(this.items, d => d.label)
+            .join(
+                enter => {
+                    const groups = enter.append('g')
+                        .attr('class', 'sme')
+                        .attr('id', d => d.label)
+                        .attr('transform', (d, i) => {return positioning.getTranslate(d, i)})
+
+                    groups.append('text')
+                        .text(d => d.label)
+                        .style('fill', d => styling.getTextColour(d))
+                        .attr('dx', 15)
+                        .attr('dy', 9)
+                    
+                    return groups
+                }
+            )
+    }
+
+}
+
+class menuItemStyling {
+    constructor(items){
+        this.items = items
+    }
+
+    getTextColour(d){
+        switch(d.type){
+            case 'title':
+                return 'blue'
+            case 'selector':
+                return 'grey'
+            default:
+                return 'black'
+        }
+    }
+}
+
+class menuItemPositioning {
+
+    constructor(items){
+        this.items = items
+    }
+
+    getTranslate(d, i){
+        const x = this.getPosX()
+        const y = this.getPosY(d, i)
+        return d3Helper.getTranslateString(x, y)
+    }
+
+    getPosX(){
+        return 10
+    }
+    
+    getPosY(d, i){
+        if(this.getSelectedItemIndex() === -1){
+            return 150 + (i + 1) * menuItem.fontSize * 2
+        } else {
+            return 150 + d.selected ? menuItem.fontSize * 2 : -50
+        }
+    }
+
+    getSelectedItemIndex(){
+        return this.items.findIndex(item => item.selected === true)
+    }
+
+}
+
+class menuItem {
+
+    static fontSize = 11
+
+    constructor(type, label){
+        this.type = type
+        this.label = label
+        this.selected = false
+    }
+}
 
 class scenario {
 
